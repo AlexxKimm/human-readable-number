d7981d2f3a0ecbf05c00bc831024e79b6f256d5a
AlexxKimm <azazel294@gmail.com>
2022-01-15 23:05:33 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
-  
+    let firstNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let secondNum = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let thirdNum = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let finite = [];
+    let hundreds = Math.floor(number / 100);
+    let duble = number % 100;
+    let single = duble % 10;
+    if (number === 0) return ['zero'];
+    if (hundreds > 0) {
+        finite.push(firstNum[hundreds]);
+        finite.push('hundred');
+    }
+    if (duble >= 20) {
+        finite.push(thirdNum[Math.floor(duble / 10)]);
+        if (single !== 0) finite.push(firstNum[single]);
+    }
+    else if (duble >= 10) finite.push(secondNum[duble % 10]);
+    else if (duble !== 0) finite.push(firstNum[single]);
+    return finite.join(' ');
 }
